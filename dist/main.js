bd47491baa60a2f41caa11df1914aa532734fec0
Joseph Kane <josephckane@gmail.com>
2017-07-18 21:50:18 -0500

    gh-pages update

---
+++ dist/main.js
@@ -0,0 +1,64 @@
+"use strict"
+
+$("document").ready(function () {
+	setLandingImage();
+});
+
+// random background images
+function setLandingImage () {
+	var imageFolder;
+	var chosenImageArray;
+
+	var fullSizeImageArray = [
+		"construction-rs",
+		"daisy-rs",
+		"dandelion-rs",
+		"forest-rs",
+		"hydrant-rs",
+		"lake-rs",
+		"river-rs",
+		"roots-rs",
+		"sunrise-rs"
+	];
+
+	var mobileImageArray = [
+		"hydrant",
+		"sunrise",
+		"lights",
+		"tree",
+		"fan",
+		"beach"
+	];
+
+	if (screen.width <= 699) {
+		imageFolder = "mobile"
+		chosenImageArray = mobileImageArray
+	} else {
+		imageFolder = "full_screen"
+		chosenImageArray = fullSizeImageArray
+	}
+
+	var randomNumber = Math.floor(Math.random() * chosenImageArray.length);
+	var randomImageName = chosenImageArray[randomNumber];
+	$(".landing").css("background", `url(dist/css/img/${imageFolder}/${randomImageName}.jpg) no-repeat center`)
+
+};
+
+// toggle section divs
+function navigateLanding (page) {
+	$(".landing").fadeOut(600)
+	$(".section").each(function () {
+		if ($(this).attr("id") == page) {
+			$(this).fadeIn(1000)
+		} else {
+			$(this).hide()
+		}
+	})
+}
+
+// go back to landing page
+function backToLanding (page) {
+	$(`.${page}`).fadeOut(600, function () {
+		$(".landing").fadeIn(1000)
+	})
+}
